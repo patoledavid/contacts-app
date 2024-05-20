014067a03cd3071249ae16a79a7b76f20dee20b9
patoledavid <patoledavid@gmail.com>
2024-05-21 09:04:01 +1000

    Contacts app for Resonate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setContacts(data));
+  }, []);
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredContacts = contacts.filter(contact => {
+    const name = contact.name.toLowerCase();
+    const searchTermLower = searchTerm.toLowerCase();
+    return name.includes(searchTermLower);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Contacts</h1>
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="Search by name"
+        />
       </header>
+      <ul>
+        {filteredContacts.map(contact => (
+          <li key={contact.id}>
+            <h2>{contact.name}</h2>
+            <p>{contact.email}</p>
+            <p>{contact.phone}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
